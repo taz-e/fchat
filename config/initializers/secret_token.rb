006b6f1a77b76383be07236b045c55dce352e4c6
tpsingh <tajinder.fzr@gmail.com>
2013-02-26 22:36:52 +0530

    Initailized Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fchat::Application.config.secret_token = '5be0976bb583d81b2b75cf65f7731de806c450efb079889ce1809c52e3a5da73c320f19a78339febd15733c9d1e8a7431ab114ecccf129dc2c33e0c38f737c58'
